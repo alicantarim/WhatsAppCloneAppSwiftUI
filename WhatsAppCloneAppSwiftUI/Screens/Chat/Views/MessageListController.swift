2fd1b5a99ab51f234e60cd4ee2c5b1403be47fb0
Alican Tarım <alicantarim@hotmail.com>
2024-05-04 15:50:01 +0300

    MessageListController and MessageListView created.

---
+++ WhatsAppCloneAppSwiftUI/Screens/Chat/Views/MessageListController.swift
@@ -0,0 +1,8 @@
+//
+//  MessageListController.swift
+//  WhatsAppCloneAppSwiftUI
+//
+//  Created by Alican TARIM on 4.05.2024.
+//
+
+import Foundation
---
+++ WhatsAppCloneAppSwiftUI/Screens/Chat/Views/MessageListView.swift
@@ -0,0 +1,18 @@
+//
+//  MessageListView.swift
+//  WhatsAppCloneAppSwiftUI
+//
+//  Created by Alican TARIM on 4.05.2024.
+//
+
+import SwiftUI
+
+struct MessageListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MessageListView()
+}
