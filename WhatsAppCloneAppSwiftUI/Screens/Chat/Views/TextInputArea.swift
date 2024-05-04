fdb4a856327e4ba7684d50074f3c5041912f2490
Alican Tarım <alicantarim@hotmail.com>
2024-05-04 15:07:50 +0300

    ChatRoomScreen created.

---
+++ WhatsAppCloneAppSwiftUI/Screens/Chat/Views/TextInputArea.swift
@@ -0,0 +1,18 @@
+//
+//  TextInputArea.swift
+//  WhatsAppCloneAppSwiftUI
+//
+//  Created by Alican TARIM on 4.05.2024.
+//
+
+import SwiftUI
+
+struct TextInputArea: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TextInputArea()
+}
