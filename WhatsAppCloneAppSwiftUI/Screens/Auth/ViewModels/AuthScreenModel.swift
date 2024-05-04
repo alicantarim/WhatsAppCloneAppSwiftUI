6bf91c980ff8713aab63d64a4396997ab53ad54d
Alican Tarım <alicantarim@hotmail.com>
2024-05-05 01:15:07 +0300

    SignUp and Login Screen created.

---
+++ WhatsAppCloneAppSwiftUI/Screens/Auth/ViewModels/AuthScreenModel.swift
@@ -0,0 +1,8 @@
+//
+//  AuthScreenModel.swift
+//  WhatsAppCloneAppSwiftUI
+//
+//  Created by Alican TARIM on 5.05.2024.
+//
+
+import Foundation
---
+++ WhatsAppCloneAppSwiftUI/Screens/Auth/Views/SignUpScreen.swift
@@ -0,0 +1,18 @@
+//
+//  SignUpScreen.swift
+//  WhatsAppCloneAppSwiftUI
+//
+//  Created by Alican TARIM on 5.05.2024.
+//
+
+import SwiftUI
+
+struct SignUpScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SignUpScreen()
+}
