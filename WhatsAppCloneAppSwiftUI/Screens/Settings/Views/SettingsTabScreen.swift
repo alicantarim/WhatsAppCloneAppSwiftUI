e74de913fc73dddf8e5d44573245f62985190564
Alican Tarım <alicantarim@hotmail.com>
2024-05-04 14:09:58 +0300

    SettingsTabScreen created.

---
+++ WhatsAppCloneAppSwiftUI/Screens/Settings/Views/SettingsTabScreen.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsTabScreen.swift
+//  WhatsAppCloneAppSwiftUI
+//
+//  Created by Alican TARIM on 4.05.2024.
+//
+
+import SwiftUI
+
+struct SettingsTabScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingsTabScreen()
+}
