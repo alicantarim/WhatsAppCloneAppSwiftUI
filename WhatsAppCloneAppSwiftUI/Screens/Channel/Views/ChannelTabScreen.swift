edadc87b394aadf486d70ec2fc063bc3190727ab
Alican Tarım <alicantarim@hotmail.com>
2024-05-04 13:19:03 +0300

    ChannelTabScreen created.

---
+++ WhatsAppCloneAppSwiftUI/Screens/Channel/Views/ChannelTabScreen.swift
@@ -0,0 +1,18 @@
+//
+//  ChannelTabScreen.swift
+//  WhatsAppCloneAppSwiftUI
+//
+//  Created by Alican TARIM on 4.05.2024.
+//
+
+import SwiftUI
+
+struct ChannelTabScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ChannelTabScreen()
+}
