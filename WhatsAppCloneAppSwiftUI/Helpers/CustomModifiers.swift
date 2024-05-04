94b2b9e3a6896b85e84acfafcf7312d89fa44ca3
Alican Tarım <alicantarim@hotmail.com>
2024-05-04 16:54:16 +0300

    BubbleTextView, BubbleTailView and MessageItem created.

---
+++ WhatsAppCloneAppSwiftUI/Helpers/CustomModifiers.swift
@@ -0,0 +1,8 @@
+//
+//  CustomModifiers.swift
+//  WhatsAppCloneAppSwiftUI
+//
+//  Created by Alican TARIM on 4.05.2024.
+//
+
+import Foundation
---
+++ WhatsAppCloneAppSwiftUI/Screens/Chat/Views/BubbleTextView.swift
@@ -0,0 +1,18 @@
+//
+//  BubbleTextView.swift
+//  WhatsAppCloneAppSwiftUI
+//
+//  Created by Alican TARIM on 4.05.2024.
+//
+
+import SwiftUI
+
+struct BubbleTextView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BubbleTextView()
+}
