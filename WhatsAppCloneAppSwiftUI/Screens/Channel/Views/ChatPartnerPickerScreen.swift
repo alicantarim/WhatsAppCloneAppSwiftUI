0ae611239ff6916edd602af115038443e4ded38c
Alican Tarım <alicantarim@hotmail.com>
2024-05-06 00:04:33 +0300

    Building ChatPartnerPickerScreen Components

---
+++ WhatsAppCloneAppSwiftUI/Screens/Channel/Views/ChatPartnerPickerScreen.swift
@@ -0,0 +1,18 @@
+//
+//  ChatPartnerPickerScreen.swift
+//  WhatsAppCloneAppSwiftUI
+//
+//  Created by Alican TARIM on 5.05.2024.
+//
+
+import SwiftUI
+
+struct ChatPartnerPickerScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ChatPartnerPickerScreen()
+}
---
+++ WhatsAppCloneAppSwiftUI/Screens/Channel/Views/ChatPartnerRowView.swift
@@ -0,0 +1,18 @@
+//
+//  ChatPartnerRowView.swift
+//  WhatsAppCloneAppSwiftUI
+//
+//  Created by Alican TARIM on 5.05.2024.
+//
+
+import SwiftUI
+
+struct ChatPartnerRowView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ChatPartnerRowView()
+}
